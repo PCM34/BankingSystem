c2f18b89994f982a1415dc7a4edf01a43fb458e4
Scott Lagler <laglerscott@gmail.com>
2017-11-25 17:29:52 -0600

    Created Menu Boilerplate

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Scanner inpt = new Scanner(System.in);
+    Database db = new Database();
 
+    // TODO Create Menu
   }
 }
