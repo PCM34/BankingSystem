7d2f3da413d264a4d7aaf36b0afab631b6c925cb
Scott Lagler <laglerscott@gmail.com>
2017-11-25 18:51:52 -0600

    Added login menu prompt

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,25 @@ public class Main {
   public static void main(String[] args) {
     Scanner inpt = new Scanner(System.in);
     Database db = new Database();
+    boolean loginSucceeded = false;
+
+    System.out.println("Welcome to Bluetech Banking");
+    System.out.println("----------------------------------------");
+
+    do {
+      System.out.println("Please Enter your information: ");
+      System.out.print("Username: ");
+      String usrName = inpt.next(); // TODO Sanitize Input
+      System.out.print("Password: ");
+      String usrPasswd = inpt.next();
+
+      if (db.checkCredentials(usrName, usrPasswd)) {
+        System.out.println("Login succeeded\n");
+        loginSucceeded = true;
+      } else {
+        System.out.println("Login failed. Please try again\n");
+      }
+    } while (!loginSucceeded);
 
     // TODO Create Menu
   }
