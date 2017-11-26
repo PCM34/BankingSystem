685fc508a537b168ac9fed2051ed5e62e8616113
Scott Lagler <laglerscott@gmail.com>
2017-11-25 19:26:25 -0600

    Added main menu prompt

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner inpt = new Scanner(System.in);
     Database db = new Database();
+    Account ac = null;
     boolean loginSucceeded = false;
 
     System.out.println("Welcome to Bluetech Banking");
@@ -19,11 +20,27 @@ public class Main {
       if (db.checkCredentials(usrName, usrPasswd)) {
         System.out.println("Login succeeded\n");
         loginSucceeded = true;
+        ac = db.getAccount(usrName);
       } else {
         System.out.println("Login failed. Please try again\n");
       }
     } while (!loginSucceeded);
 
-    // TODO Create Menu
+    String choice;
+
+    System.out.println("Welcome " + ac.getFirstName() + " " + ac.getLastName());
+    System.out.println("Account number:" + ac.getAccountNum());
+    System.out.println("----------------------------------------");
+    System.out.println("  1. View Balance");
+    System.out.println("  2. Transfer funds"); // More options to be implemented
+    System.out.println("  3. Delete account");
+    System.out.print("Please select an option: ");
+    choice = inpt.next();
+
+    switch (choice) {
+      case "1": // TODO Create main menu logic
+        break;
+    }
+    // TODO Create option to create account.
   }
 }
