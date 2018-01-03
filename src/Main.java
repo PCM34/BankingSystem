c1773b4fe67028d3982ac5722816a7094e076720
Scott Lagler <laglerscott@gmail.com>
2018-01-02 23:07:52 -0600

    Fixed formatting

--- src/Main.java
+++ src/Main.java
@@ -27,44 +27,44 @@ public class Main {
       Account act = new Account(fullName, usrName, usrPasswd);
       db.addAccount(act);
     }
-     do {
-       System.out.println("Please Enter your Login information: ");
-       System.out.print("Username: ");
-       String usrName = inpt.next().trim();
-       System.out.print("Password: ");
-       String usrPasswd = inpt.next();
+    do {
+      System.out.println("Please Enter your Login information: ");
+      System.out.print("Username: ");
+      String usrName = inpt.next().trim();
+      System.out.print("Password: ");
+      String usrPasswd = inpt.next();
 
-       ac = main.authenticate(usrName, usrPasswd);
-     } while (!loginSucceeded);
+      ac = main.authenticate(usrName, usrPasswd);
+    } while (!loginSucceeded);
 
-     String choice;
-     System.out.println("Welcome " + ac.getFirstName() + " " + ac.getLastName());
-     System.out.println("Account number:" + ac.getAccountNum());
-     System.out.println("----------------------------------------");
-     System.out.println("  1. View Balance");
-     System.out.println("  2. Transfer funds"); // More options to be implemented
-     System.out.println("  3. Delete account");
-     System.out.print("Please select an option: ");
-     choice = inpt.next();
+    String choice;
+    System.out.println("Welcome " + ac.getFirstName() + " " + ac.getLastName());
+    System.out.println("Account number:" + ac.getAccountNum());
+    System.out.println("----------------------------------------");
+    System.out.println("  1. View Balance");
+    System.out.println("  2. Transfer funds"); // More options to be implemented
+    System.out.println("  3. Delete account");
+    System.out.print("Please select an option: ");
+    choice = inpt.next();
 
-     switch (choice) {
-       case "1": // TODO Create main menu logic
-         System.out.println("Balance: $" + ac.getBalance());
-         break;
-       case "2":
-         break;
-       case "3":
-         if (db.delAccount(ac))
-           System.out.println("Account successfully deleted");
-         else
-           System.out.println("Deletion failed, please try again.");
-         break;
-       default:
-         System.out.println("Invalid menu option");
-         break;
-     }
-   }
-   // TODO Create option to create account.
+    switch (choice) {
+      case "1": // TODO Create main menu logic
+        System.out.println("Balance: $" + ac.getBalance());
+        break;
+      case "2":
+        break;
+      case "3":
+        if (db.delAccount(ac))
+          System.out.println("Account successfully deleted");
+        else
+          System.out.println("Deletion failed, please try again.");
+        break;
+      default:
+        System.out.println("Invalid menu option");
+        break;
+    }
+  }
+  // TODO Create option to create account.
 
   private Account authenticate(String usrName, String usrPasswd) {
     if (db.checkCredentials(usrName, usrPasswd)) {
