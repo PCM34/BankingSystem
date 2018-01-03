66ca7fe975e76dfb7850525d8da4907af3b58505
Scott Lagler <laglerscott@gmail.com>
2018-01-02 23:05:18 -0600

    Added account search

--- src/Database.java
+++ src/Database.java
@@ -1,30 +1,38 @@
 import java.util.ArrayList;
 
-public class Database {
+class Database {
   private ArrayList<Account> dataList;
 
   Database() {
     dataList = new ArrayList<>();
   }
 
-  public boolean addAccount(Account ac) {
+  boolean addAccount(Account ac) {
     return dataList.add(ac);
   }
 
-  public boolean delAccount(Account ac) {
+  boolean delAccount(Account ac) {
     return dataList.remove(ac);
     // TODO Check if delAccount works for given context
   }
 
-  public Account getAccount(String UserName) {
+  Account getAccount(String UserName) {
     for (Account DataListIndex : dataList)
       if (DataListIndex.getUserName().equals(UserName)) return DataListIndex;
     return null;
   }
 
-  public boolean checkCredentials(String UserName, String passwd) {
+  boolean checkCredentials(String UserName, String passwd) {
     for (Account ac:dataList)
       if (ac.getUserName().equals(UserName) && ac.getPassword().equals(passwd)) return true;
     return false;
+  }
+
+  Account searchAccount(int acctNum) {
+    for (Account ac : dataList) {
+      if (ac.getAccountNum() == acctNum)
+        return ac;
+    }
+    return null;
   }
 }
