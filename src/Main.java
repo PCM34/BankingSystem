261105ead929e8778e19f75b36204cddab49a8a1
Scott Lagler <laglerscott@gmail.com>
2018-01-14 23:00:19 -0600

    Made main menu loopable & fixed logout

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,12 @@ public class Main {
     System.out.println("  2. Sign up");
     System.out.print("Please select an option: ");
     String loginChoice = inpt.next();
+    String choice = "";
 
     if (loginChoice.equals("2")) {
       System.out.println("Please Enter your information: ");
       System.out.print("Full Name: ");
-      String fullName = inpt.next();
+      String fullName = inpt.nextLine();
       inpt.next();
       System.out.print("Username: ");
       String usrName = inpt.next().trim();
@@ -38,40 +39,45 @@ public class Main {
       ac = main.authenticate(usrName, usrPasswd);
     } while (!loginSucceeded);
 
-    String choice;
-    System.out.println("Welcome " + ac.getFullName());
-    System.out.println("Account number:" + ac.getAccountNum());
-    System.out.println("----------------------------------------");
-    System.out.println("  1. View Balance");
-    System.out.println("  2. Transfer funds"); // More options to be implemented
-    System.out.println("  3. Delete account");
-    System.out.print("Please select an option: ");
-    choice = inpt.next();
+    do {
+      System.out.println("Welcome " + ac.getFullName());
+      System.out.println("Account number:" + ac.getAccountNum());
+      System.out.println("----------------------------------------");
+      System.out.println("  1. View Balance");
+      System.out.println("  2. Transfer funds"); // More options to be implemented
+      System.out.println("  3. Delete account");
+      System.out.println("  4. Exit/Logout");
+      System.out.print("Please select an option: ");
+      choice = inpt.next();
 
-    switch (choice) {
-      case "1":
-        System.out.println("Balance: $" + ac.getBalance());
-        break;
-      case "2":
-        System.out.println("Transfer Funds:\n----------------------------------------");
-        System.out.println("Current account number: " + ac.getAccountNum());
-        System.out.print("Destination account number: ");
-        int reciveNum = inpt.nextInt();
-        System.out.print("Amount: $");
-        double amnt = inpt.nextDouble();
+      switch (choice) {
+        case "1":
+          System.out.println("Balance: $" + ac.getBalance());
+          break;
+        case "2":
+          System.out.println("Transfer Funds:\n----------------------------------------");
+          System.out.println("Current account number: " + ac.getAccountNum());
+          System.out.print("Destination account number: ");
+          int reciveNum = inpt.nextInt();
+          System.out.print("Amount: $");
+          double amnt = inpt.nextDouble();
 
-        main.transferFunds(reciveNum, amnt);
-        break;
-      case "3":
-        if (db.delAccount(ac))
-          System.out.println("Account successfully deleted");
-        else
-          System.out.println("Deletion failed, please try again.");
-        break;
-      default:
-        System.out.println("Invalid menu option");
-        break;
-    }
+          main.transferFunds(reciveNum, amnt);
+          break;
+        case "3":
+          if (db.delAccount(ac))
+            System.out.println("Account successfully deleted");
+          else
+            System.out.println("Deletion failed, please try again.");
+          break;
+        case "4":
+          System.out.println("Thank you for using BlueTech Banking!");
+          break;
+        default:
+          System.out.println("Invalid menu option");
+          break;
+      }
+    } while (!(choice.equals("4") || choice.equals("3")));
   }
 
   private Account authenticate(String usrName, String usrPasswd) {
